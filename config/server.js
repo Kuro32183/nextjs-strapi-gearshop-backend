87c945cc08cc1194cb5445ff792be44796beee8d
Kuro32183 <takumi32183@gmail.com>
2022-07-23 23:57:16 +0900

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd648f2ca9215e5ca590ca43009626ac3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'edf0f82b-faf6-48f7-a735-bec56d42f835'
+};
