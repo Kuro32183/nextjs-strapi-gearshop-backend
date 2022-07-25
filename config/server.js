9d704c62f0a4e5f6a15f587cd6db87a8620f6fb9
Kuro32183 <takumi32183@gmail.com>
2022-07-26 00:59:47 +0900

    new

--- config/server.js
+++ config/server.js
@@ -7,11 +7,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-});
